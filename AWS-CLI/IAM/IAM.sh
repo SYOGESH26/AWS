b752fdb0e1d616c4e4c78fe7e277a7060ba526e7
root <root@YOGESH.localdomain>
2025-06-16 12:29:04 +0530

    In this I explain about IAM

---
+++ AWS-CLI/IAM/IAM.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Create IAM users
+
+aws iam create-user --user-name yogesh 
+
+#Create Group 
+
+aws iam create-group --group-name Engineers
+
+#Attach policy to group.
+
+aws iam attach-group-policy \
+	--group-name Engineers \
+	--policy-arn arn:aws::iam:aws:policy/AmazonEc2FullAccess 
+
+#Add user to group
+
+aws iam add-user-to-group \
+	--user-name yogesh \
+	--group-name Engineers
+
+#Create access keys to user
+
+aws iam create-access-key --user-name yogesh
